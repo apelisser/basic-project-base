4ddc3974ee4c01f6a093ef3ab5e7f4163b94876d
Abner J Pelisser <abner.pelisser@gmail.com>
2024-07-18 15:44:57 -0300

    init: initial commit

---
+++ src/test/java/com/apelisser/base/BaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apelisser.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
